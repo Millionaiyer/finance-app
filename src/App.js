fd2edad4031823db39f83e1ebcbb1a40fbb97938
Millionaiyer <surajsaiyer@gmail.com>
2022-12-11 13:26:11 +0530

    Assignement section4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import Expenses from './Components/Expenses';
+import React from 'react';
 
-function App() {
+import NewExpense from './Components/NewExpense/NewExpense';
+import Expenses from './Components/Expenses/Expenses';
+
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -23,7 +26,24 @@ function App() {
     },
   ];
 
-  return <Expenses items={expenses} />;
-}
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
 
 export default App;
